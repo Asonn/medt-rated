a61084e92ec85185060eb7b8a66cdab6b07b038f
Cyan-Thaiger <hajredegeld@casema.nl>
2018-01-23 16:28:54 +0100

    Update sensors.py

--- sensors.py
+++ sensors.py
@@ -12,8 +12,8 @@ RightSensor = 24
 ButtonPin = 21
 
 # TurnMotor pins, defines wheels
-LeftWheel = [4, 17, 27, 22]
-RightWheel = [26, 19, 13, 9]
+LeftWheel = [4,17,27,22]
+RightWheel = [26,19,13,9]
 
 #setup input sensors
 GPIO.setup(LeftSensor, GPIO.IN)
@@ -31,93 +31,142 @@ prev_state = 1
 
 # Definieer variabelen.
 StepCounter = 0
-StepCount = 8;
 
 # "Vooruit"
-Seq2 = []
-Seq2 = range(0, StepCount)
-Seq2[0] = [1,0,0,0]
-Seq2[1] = [1,1,0,0]
-Seq2[2] = [0,1,0,0]
-Seq2[3] = [0,1,1,0]
-Seq2[4] = [0,0,1,0]
-Seq2[5] = [0,0,1,1]
-Seq2[6] = [0,0,0,1]
-Seq2[7] = [1,0,0,1]
+StepCount1 = 8
+Seq1 = []
+Seq1 = range(0, StepCount1)
+Seq1[0] = [1,0,0,0]
+Seq1[1] = [1,1,0,0]
+Seq1[2] = [0,1,0,0]
+Seq1[3] = [0,1,1,0]
+Seq1[4] = [0,0,1,0]
+Seq1[5] = [0,0,1,1]
+Seq1[6] = [0,0,0,1]
+Seq1[7] = [1,0,0,1]
 
 # "Achteruit"
-Seq3 = []
-Seq3 = range(0, StepCount)
-Seq3[0] = [0,0,0,1]
-Seq3[1] = [0,0,1,1]
-Seq3[2] = [0,0,1,0]
-Seq3[3] = [0,1,1,0]
-Seq3[4] = [0,1,0,0]
-Seq3[5] = [1,1,0,0]
-Seq3[6] = [1,0,0,0]
-Seq3[7] = [1,0,0,1]
+StepCount2 = 8
+Seq2 = []
+Seq2 = range(0, StepCount2)
+Seq2[0] = [0,0,0,1]
+Seq2[1] = [0,0,1,1]
+Seq2[2] = [0,0,1,0]
+Seq2[3] = [0,1,1,0]
+Seq2[4] = [0,1,0,0]
+Seq2[5] = [1,1,0,0]
+Seq2[6] = [1,0,0,0]
+Seq2[7] = [1,0,0,1]
 
 # Welke stappenvolgorde gaan we hanteren?
-Seq = Seq3
-Seq2 = Seq2
+# Seq1 = Seq3
+# Seq2 = Seq2
 
+StepCount = StepCount1
 
-def drive():
-    # go forward
-    for pin in range(0, 4):
-      xpin = RightWheel[pin]
-      ypin = LeftWheel[pin]
-
-      if (direction = "w"):
-          GPIO.output(xpin, Seq2[StepCounter][pin])
-          GPIO.output(ypin, Seq1[StepCounter][pin])
-          count()
-          # Wait for the next sequence (lower = faster)
-          sleep(.001)
-      elif (direction = "l")
-           GPIO.output(xpin, Seq2[StepCounter][pin])
-           sleep(.003)
-           GPIO.output(ypin, Seq1[StepCounter][pin])
-           count()
-           # Wait for the next sequence (lower = faster)
-           sleep(.001)
-
-def count:
-    StepCounter += 1
-
-    # restart sequence at the end.
-    if (StepCounter==StepCount): StepCounter = 0
-    if (StepCounter<0): StepCounter = StepCount
 
 try:
     while True:
         curr_state = GPIO.input(ButtonPin)
+
         # print("middle %s" % GPIO.input(MiddleSensor))
 
         while (curr_state != prev_state):
             # once pressed set it back to previous state, for an infinite loop
-            curr_state = prev_state
+            curr_state = 0
+            StopCounter = 0
 
             # if there is no light for the middle ONLY
             if GPIO.input(LeftSensor) and GPIO.input(RightSensor) and not GPIO.input(MiddleSensor):
                 # go forward
-                drive("w")
-                stopcount = 0
-            elif GPIO.input(LeftSensor) and not GPIO.input(RightSensor) and not GPIO.input(MiddleSensor)
-            or GPIO.input(LeftSensor) and not GPIO.input(RightSensor):
-                drive("r")
-                stopcount = 0
-            elif not GPIO.input(LeftSensor) and GPIO.input(RightSensor) and not GPIO.input(MiddleSensor)
-            or not GPIO.input(LeftSensor) and GPIO.input(RightSensor):
-                drive("l")
-                stopcount = 0
-            elif GPIO.input(LeftSensor) and GPIO.input(RightSensor) and GPIO.input(MiddleSensor):
-                #keep driving the same direction.
-                stopcount = 0
-            else
-                stopcount += 1
+                for pin in range(0, 4):
+                  xpin = RightWheel[pin]
+                  ypin = LeftWheel[pin]
+                  GPIO.output(xpin, Seq2[StepCounter][pin])
+                  GPIO.output(ypin, Seq1[StepCounter][pin])
+                    # GPIO.output(xpin, False)
+                    # GPIO.output(ypin, False)
+                StepCounter += 1
+
+                # Als we aan het einde van de stappenvolgorde zijn beland start dan opnieuw
+                if (StepCounter==StepCount): StepCounter = 0
+                if (StepCounter<0): StepCounter = StepCount
+
+                # Wacht voor de volgende stap (lager = snellere draaisnelheid)
+                sleep(.001)
+
+                print("Middle")
+                # if GPIO.input(ButtonPin) == 0:
+                #     curr_state = GPIO.input(ButtonPin)
+                StopCounter = 0
+
+            if GPIO.input(LeftSensor) and GPIO.input(RightSensor) and GPIO.input(MiddleSensor) and StopCounter < 1000:
+                for pin in range(0, 4):
+                  xpin = RightWheel[pin]
+                  ypin = LeftWheel[pin]
+                  GPIO.output(xpin, Seq2[StepCounter][pin])
+                  GPIO.output(ypin, Seq1[StepCounter][pin])
+                    # GPIO.output(xpin, False)
+                    # GPIO.output(ypin, False)
+                StepCounter += 1
+
+                # Als we aan het einde van de stappenvolgorde zijn beland start dan opnieuw
+                if (StepCounter==StepCount): StepCounter = 0
+                if (StepCounter<0): StepCounter = StepCount
+
+                # Wacht voor de volgende stap (lager = snellere draaisnelheid)
+                sleep(.001)
+                StopCounter += 1
 
 
+
+            if GPIO.input(LeftSensor) and not GPIO.input(RightSensor):
+                # go right
+                for pin in range(0, 4):
+                  xpin = RightWheel[pin]
+                  ypin = LeftWheel[pin]
+                  GPIO.output(xpin, Seq1[StepCounter][pin])
+                  GPIO.output(ypin, Seq1[StepCounter][pin])
+                    # GPIO.output(xpin, False)
+                    # GPIO.output(ypin, False)
+                StepCounter += 1
+
+                # Als we aan het einde van de stappenvolgorde zijn beland start dan opnieuw
+                if (StepCounter==StepCount): StepCounter = 0
+                if (StepCounter<0): StepCounter = StepCount
+
+                # Wacht voor de volgende stap (lager = snellere draaisnelheid)
+                sleep(.001)
+
+                StopCounter = 0
+
+            if GPIO.input(RightSensor) and not GPIO.input(LeftSensor):
+                # go left
+                for pin in range(0, 4):
+                  xpin = RightWheel[pin]
+                  ypin = LeftWheel[pin]
+                  GPIO.output(xpin, Seq2[StepCounter][pin])
+                  GPIO.output(ypin, Seq2[StepCounter][pin])
+                    # GPIO.output(xpin, False)
+                    # GPIO.output(ypin, False)
+                StepCounter += 1
+
+                # Als we aan het einde van de stappenvolgorde zijn beland start dan opnieuw
+                if (StepCounter==StepCount): StepCounter = 0
+                if (StepCounter<0): StepCounter = StepCount
+
+                # Wacht voor de volgende stap (lager = snellere draaisnelheid)
+                sleep(.001)
+
+                StopCounter = 0
+
+            # if GPIO.input(MiddleSensor) == True:
+            #     print("Middle")
+            #     sleep(.5)
+            #
+            # if GPIO.input(RightSensor) == True:
+            #     print("Right")
+            #     sleep(.5)
 
 except KeyboardInterrupt:
   # GPIO netjes afsluiten
